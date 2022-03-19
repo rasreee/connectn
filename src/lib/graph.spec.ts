a8860468e689959f97ea211198d574bff1d89d24
Lesley Chang <lesley.hanna98@gmail.com>
2022-03-19 16:28:24 -0400

    wip fix tests

--- src/lib/graph.spec.ts
+++ src/lib/graph.spec.ts
@@ -7,41 +7,36 @@ import { Point } from './point';
  * @group graph
  */
 describe('lib/graph', () => {
-  it('initializes', () => {
-    const grid = new Graph(2, 2);
-    expect(grid.columnCount).toEqual(2);
-    expect(grid.rowCount).toEqual(2);
-    expect(grid.points.every((point) => point.value === null)).toBeTruthy();
-  });
-
-  it('getLinesIncluding()', () => {
-    const grid = new Graph(2, 2);
-    const target: Point = new Point(1, 0);
-
-    const lines = grid.getLinesIncluding(target);
-
-    expect(lines.length).toEqual(1);
-    expect(lines[0].length).toEqual(3);
-  });
-
-  it('discoverLine()', () => {
-    const grid = new Graph(4, 4);
-    const initial = [new Point(0, 0), new Point(1, 0)];
+  let grid = new Graph(4, 4);
+  beforeEach(() => {
+    grid = new Graph(4, 4);
     grid.addPoint(0, 0, 'A');
     grid.addPoint(1, 0, 'B');
     grid.addPoint(2, 0, 'C');
     grid.addPoint(3, 0, 'D');
+  });
+
+  it('initializes', () => {
+    const result = new Graph(2, 2);
+    expect(result.columnCount).toEqual(2);
+    expect(result.rowCount).toEqual(2);
+    expect(result.points.every((point) => point.value === null)).toBeTruthy();
+  });
+
+  it('getLongestLine()', () => {
+    const line = grid.getLongestLine();
+    expect(line.length).toEqual(4);
+  });
+
+  it('discoverLine()', () => {
+    const initial = [new Point(0, 0), new Point(1, 0)];
     const result = grid.discoverLine(initial);
     const coordsTuples = result.map((point) => toCoordsTuple(point.coords));
-    expect(coordsTuples).toEqual(
-      [
-        [0, 0],
-        [1, 0],
-        [2, 0],
-        [3, 0],
-      ].map((coords, index) => new Point(coords[0], coords[1], index))
-    );
-
-    grid.clear();
+    expect(coordsTuples).toEqual([
+      [0, 0],
+      [1, 0],
+      [2, 0],
+      [3, 0],
+    ]);
   });
 });
