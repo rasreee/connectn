3792d18a1adc0a017979cf1b1eaabee72fe0ff94
Lesley Chang <lesley.hanna98@gmail.com>
2022-03-19 06:44:41 -0400

    fix: board info text

--- src/Board/BoardInfo.tsx
+++ src/Board/BoardInfo.tsx
@@ -5,11 +5,15 @@ import { useMemo } from 'react';
 export const BoardInfo = () => {
   const { info, state } = useGame();
 
+  const getPlayerName = (playerIndex: number | undefined) =>
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    [info.playerOneName, info.playerTwoName][playerIndex!];
+
   const text = useMemo(() => {
     const outcome = getGameOutcome({ info, state });
 
     if (outcome?.type === OutcomeType.Winner)
-      return `${outcome.winner} is ${OutcomeType.Winner}!`;
+      return `${getPlayerName(outcome.winner)} is ${OutcomeType.Winner}!`;
 
     if (outcome?.type === OutcomeType.Draw) return OutcomeType.Draw;
 
