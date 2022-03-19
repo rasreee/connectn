97cc9eb93178618309257e41b8ab439bb4eb6786
Lesley Chang <lesley.hanna98@gmail.com>
2022-03-18 22:31:22 -0400

    refactor: board to fc

--- src/Board/Board.tsx
+++ src/Board/Board.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Board.css';
 import { GameInfo, GameState, Piece } from '../Game/types';
 import { BoardPiece } from './BoardPiece';
@@ -16,49 +16,43 @@ interface BoardProps {
   placePiece: (column: number, row: number) => void;
 }
 
-export class Board extends Component<BoardProps> {
-  render() {
-    const { gameInfo, gameState } = this.props;
-
-    return (
-      <>
-        <BoardInfo gameInfo={gameInfo} gameState={gameState} />
-        <div
-          className='Board'
-          style={{
-            width: gameInfo.columnCount * 50,
-            height: gameInfo.rowCount * 50,
-          }}
-        >
-          {/** TODO(2): placing game pieces
-           * - how do utilize the provided board piece component to visualize the game state?
-           */}
-          {gameState.pieces.map((piece, i) => (
-            <BoardPiece
-              key={i}
-              column={piece.column}
-              row={piece.row}
-              gameInfo={gameInfo}
-              color={getPieceColor(piece, gameInfo)}
-            />
-          ))}
-          {Array.from(Array(gameInfo.rowCount), (e, row) => (
-            <div key={`row-${row}`} className='Board-Row'>
-              {Array.from(Array(gameInfo.columnCount), (e, column) => (
-                <div
-                  key={`slot-${column}-${row}`}
-                  className='Board-Slot'
-                  onClick={() =>
-                    this.props.placePiece(column, gameInfo.rowCount - row - 1)
-                  }
-                >
-                  {row}, {column}
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
-      </>
-    );
-  }
-}
+export const Board = ({ gameInfo, gameState, placePiece }: BoardProps) => {
+  return (
+    <>
+      <BoardInfo gameInfo={gameInfo} gameState={gameState} />
+      <div
+        className='Board'
+        style={{
+          width: gameInfo.columnCount * 50,
+          height: gameInfo.rowCount * 50,
+        }}
+      >
+        {/** TODO(2): placing game pieces
+         * - how do utilize the provided board piece component to visualize the game state?
+         */}
+        {gameState.pieces.map((piece, i) => (
+          <BoardPiece
+            key={i}
+            column={piece.column}
+            row={piece.row}
+            gameInfo={gameInfo}
+            color={getPieceColor(piece, gameInfo)}
+          />
+        ))}
+        {Array.from(Array(gameInfo.rowCount), (e, row) => (
+          <div key={`row-${row}`} className='Board-Row'>
+            {Array.from(Array(gameInfo.columnCount), (e, column) => (
+              <div
+                key={`slot-${column}-${row}`}
+                className='Board-Slot'
+                onClick={() => placePiece(column, gameInfo.rowCount - row - 1)}
+              >
+                {row}, {column}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
