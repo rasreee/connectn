0b6ee1009ebc81a3b01b04d56b1eebe461e9ab78
Lesley Chang <lesley.hanna98@gmail.com>
2022-03-20 16:26:08 -0400

    feat: button styling

---
+++ src/components/Game/GameStatusIndicator.tsx
@@ -0,0 +1,33 @@
+import { computed } from 'mobx';
+import { observer } from 'mobx-react-lite';
+import { GameOutcome } from 'models/gameState';
+import { useGameInfo, useGameState } from 'stores/hooks';
+
+import styles from './GameStatusIndicator.module.css';
+
+export const GameStatusIndicator = observer(function GameStatusIndicator() {
+  const gameInfo = useGameInfo();
+  const gameState = useGameState();
+
+  const getPlayerName = (playerIndex: number | undefined) =>
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    [gameInfo.playerOneName, gameInfo.playerTwoName][playerIndex!];
+
+  const text = computed(() => {
+    switch (gameState.outcome) {
+      case GameOutcome.Draw:
+        return 'Draw';
+      case GameOutcome.Win:
+        return `${getPlayerName(gameState.winner)} is Winner!`;
+      default:
+        return `${getPlayerName(gameState.currentPlayer)}'s turn`;
+    }
+  }).get();
+
+  return (
+    <div className={styles.statusIndicator}>
+      <div className={styles.circle} />
+      <span className={styles.statusIndicator_text}>{text}</span>
+    </div>
+  );
+});
--- src/components/Game/GameTopBar.tsx
+++ src/components/Game/GameTopBar.tsx
@@ -1,43 +1,20 @@
-import './GameTopBar.css';
-
-import { computed } from 'mobx';
 import { observer } from 'mobx-react-lite';
-import { GameOutcome } from 'models/gameState';
-import { useGameInfo, useGameState } from 'stores/hooks';
+import { useGameState } from 'stores/hooks';
 
-const StatusIndicator = observer(function StatusIndicator() {
-  const gameInfo = useGameInfo();
-  const gameState = useGameState();
-
-  const getPlayerName = (playerIndex: number | undefined) =>
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    [gameInfo.playerOneName, gameInfo.playerTwoName][playerIndex!];
-
-  const text = computed(() => {
-    switch (gameState.outcome) {
-      case GameOutcome.Draw:
-        return 'Draw';
-      case GameOutcome.Win:
-        return `${getPlayerName(gameState.winner)} is Winner!`;
-      default:
-        return `${getPlayerName(gameState.currentPlayer)}'s turn`;
-    }
-  }).get();
-
-  return (
-    <div className='StatusIndicator'>
-      <div className='text'>{text}</div>
-    </div>
-  );
-});
+import { GameStatusIndicator } from './GameStatusIndicator';
+import styles from './GameTopBar.module.css';
 
 export const GameTopBar = observer(() => {
   const gameState = useGameState();
 
   return (
-    <div className='GameTopBar'>
-      <StatusIndicator />
-      {gameState.winner && <button onClick={gameState.reset}>New Game</button>}
+    <div className={styles.container}>
+      <GameStatusIndicator />
+      {gameState.winner && (
+        <button className={styles.button} onClick={gameState.reset}>
+          New Game
+        </button>
+      )}
     </div>
   );
 });
