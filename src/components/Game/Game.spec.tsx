124bac353943163cffae038e054e43f2a063dbf5
Lesley Chang <lesley.hanna98@gmail.com>
2022-03-22 00:19:29 -0400

    cleanup

--- src/components/Game/Game.spec.tsx
+++ src/components/Game/Game.spec.tsx
@@ -6,7 +6,7 @@ import { PlayerColor } from 'lib/player'
 import { muteConsole } from 'test-utils/muteConsole'
 import { render } from 'test-utils/react'
 
-import { Game } from '.'
+import { Game } from './Game'
 
 /**
  * @group components/Game
@@ -31,50 +31,25 @@ describe('components/Game', () => {
     expect(pieceEl).toBeVisible()
     expect(pieceEl).toHaveStyle(`color: ${PlayerColor.PlayerOne};`)
 
-    slotEl = result.getByRole(`slot-1-0`)
-    userEvent.click(slotEl)
-    pieceEl = result.getByRole(`piece-1-0`)
-    expect(pieceEl).toBeVisible()
-    expect(pieceEl).toHaveStyle(`color: ${PlayerColor.PlayerTwo};`)
-
     slotEl = result.getByRole(`slot-0-1`)
     userEvent.click(slotEl)
     pieceEl = result.getByRole(`piece-0-1`)
     expect(pieceEl).toBeVisible()
-    expect(pieceEl).toHaveStyle(`color: ${PlayerColor.PlayerOne};`)
+    expect(pieceEl).toHaveStyle(`color: ${PlayerColor.PlayerTwo};`)
+    // click same slot again - should stay the same color
+    userEvent.click(slotEl)
+    expect(pieceEl).toHaveStyle(`color: ${PlayerColor.PlayerTwo};`)
 
+    // fill up the rest of the board
+    slotEl = result.getByRole(`slot-1-0`)
+    userEvent.click(slotEl)
+    pieceEl = result.getByRole(`piece-1-0`)
+    expect(pieceEl).toBeVisible()
+    expect(pieceEl).toHaveStyle(`color: ${PlayerColor.PlayerOne};`)
     slotEl = result.getByRole(`slot-1-1`)
     userEvent.click(slotEl)
     pieceEl = result.getByRole(`piece-1-1`)
     expect(pieceEl).toBeVisible()
     expect(pieceEl).toHaveStyle(`color: ${PlayerColor.PlayerTwo};`)
-  })
-
-  describe('given column is full and column is clicked', () => {
-    it('should remain the same', () => {
-      const gameInfo = createGameInfo({ columnCount: 2, rowCount: 2 })
-      const result = render(<Game gameInfo={gameInfo} />)
-
-      let slotEl = result.getByRole(`slot-0-0`)
-      userEvent.click(slotEl)
-      let pieceEl = result.getByRole(`piece-0-0`)
-      expect(pieceEl).toBeVisible()
-      expect(pieceEl).toHaveStyle(`color: ${PlayerColor.PlayerOne};`)
-
-      slotEl = result.getByRole(`slot-0-1`)
-      userEvent.click(slotEl)
-      pieceEl = result.getByRole(`piece-0-1`)
-      expect(pieceEl).toBeVisible()
-      expect(pieceEl).toHaveStyle(`color: ${PlayerColor.PlayerTwo};`)
-
-      slotEl = result.getByRole(`slot-0-0`)
-      userEvent.click(slotEl)
-      pieceEl = result.getByRole(`piece-0-0`)
-      expect(pieceEl).toBeVisible()
-      expect(pieceEl).toHaveStyle(`color: ${PlayerColor.PlayerOne};`)
-      pieceEl = result.getByRole(`piece-0-1`)
-      expect(pieceEl).toBeVisible()
-      expect(pieceEl).toHaveStyle(`color: ${PlayerColor.PlayerTwo};`)
-    })
   })
 })
